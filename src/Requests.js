c7b0b015f3cee6d9d98d38caff21433ffcff64ce
HelbachJ <jthelbach@gmail.com>
2023-05-19 06:14:50 -0500

    Added more features to the Netflix app

--- src/Requests.js
+++ src/Requests.js
@@ -4,7 +4,7 @@ const requests = {
     requestPopular :`https://api.themoviedb.org/3/movie/popular?api_key=${key}&languages=en-US&page=1`,
     requestTopRated : `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&languages=en-US&page=1`,
     requestTrending : `https://api.themoviedb.org/3/movie/popular?api_key=${key}&languages=en-US&page=2`,
-    requestHorror : `https://api.themoviedb.org/3/movie/movie?api_key=${key}&languages=en-US&query=horror&page=1&include`,
+    //requestHorror : `https://api.themoviedb.org/3/movie/movie?api_key=${key}&languages=en-US&query=horror&page=1&include`,
     requestUpcoming : `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&languages=en-US&page=1`,
 };
 
