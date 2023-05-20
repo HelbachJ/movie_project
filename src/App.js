8d4446b15d57a76e1aedcc3a6b8edbf385db06cb
HelbachJ <jthelbach@gmail.com>
2023-05-20 02:55:31 -0500

    Sign-In/Sign-Up Authorization

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import Navbar from "./components/Navbar";
-import {Route, Routes} from "react-router-dom"
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-
+import {AuthContextProvider} from "./context/AuthContext";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Account from "./pages/Account";
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Routes>
-      <Route path='/' element={<Home/>} />
-    </Routes>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/account" element={<Account />} />
+        </Routes>
+      </AuthContextProvider>
     </>
   );
 }
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import { createContext, useContext, useEffect, useState } from "react";
+import React from "react";
+import { auth } from "../Firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
 
-const AuthContext = () => {
+const AuthContext = createContext();
+
+export function AuthContextProvider({ children }) {
+  const [user, setUser] = useState({});
+
+  function signUp(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
+
+  function signIn(email, password) {
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+  function logOut() {
+    return signOut(auth);
+  }
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currUser) => {
+      setUser(currUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   return (
-    <div>AuthContext</div>
-  )
+    <AuthContextProvider value={{ signUp, signIn, logOut, user }}>
+      {children}
+    </AuthContextProvider>
+  );
 }
 
-export default AuthContext
+export function UserAuth() {
+  return useContext(AuthContext);
+}
